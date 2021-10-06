9a3c0ce3975d66b8b6b91aa439291b82a2bdc8e8
kirilko <chipzz87@gmail.com>
2021-10-06 14:34:51 +0300

    simple fixes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 from flask import Flask, request, render_template, make_response, redirect
 import csv
 
@@ -17,43 +16,47 @@ def read_csv():
 
 @app.route("/", methods=['GET'])
 def index():
-    data = read_csv()
-    # args = [i.split('=')[1] for i in request.query_string.decode("utf-8").split('&')]
-    args = request.args.getlist('category')
-    images = []
-    if args:  # изображения по указанным категориям
-        for i in data:
-            for j in args:
-                if j in i[2:] and i[:2] not in images:
-                    images.append(i[:2])
-    else:  # произвольные изображения
-        for i in data:
-            images.append(i[:2])
-    while images:
-        rnd = 0
-        if len(images) > 1:
-            rnd = random.randint(0, len(images) - 1)
-        cnt = int(images[rnd][1]) - 1
-        if request.cookies.get(images[rnd][0]):
-            cnt = int(request.cookies.get(images[rnd][0])) - 1
-        if cnt < 0:
-            images.pop(rnd)
-            cnt = 0
+    image_url_index = 0
+    image_shows_amount_index = 1
+    image_categories_index = 2
+    min_images_len = 1
+
+    csv_data = read_csv()
+    categories = request.args.getlist('category')
+    images_to_show = []
+    if categories:
+        for image in csv_data:
+            for category in categories:
+                if category in image[image_categories_index:] and image[:image_categories_index] not in images_to_show:
+                    images_to_show.append(image[:image_categories_index])
+    else:
+        for image in csv_data:
+            images_to_show.append(image[:image_categories_index])
+    current_image = ''
+    shows_count = 0
+    while images_to_show:
+        current_image_index = random.randint(0, len(images_to_show) - 1) if len(images_to_show) > min_images_len else 0
+        shows_count = int(request.cookies.get(images_to_show[current_image_index][image_url_index],
+                                              images_to_show[current_image_index][image_shows_amount_index]))
+        current_image = images_to_show[current_image_index][image_url_index]
+        shows_count -= 1
+        if shows_count < 0:
+            images_to_show.pop(current_image_index)
+            shows_count = 0
+            current_image = ''
         else:
             break
-    image = ''
-    if images:
-        image = images[rnd][0]
-    res = make_response(render_template('index.html', image=image, count=cnt,
-                                        query=list(map(lambda x: f'category={x}', args))))
-    if images:
-        res.set_cookie(image, str(cnt))
-    return res
+    response = make_response(render_template('index.html', image=current_image, count=shows_count,
+                                             query=list(map(lambda x: f'category={x}', categories))))
+    if images_to_show:
+        response.set_cookie(current_image, str(shows_count))
+    return response
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.run('127.0.0.1', 8000)
